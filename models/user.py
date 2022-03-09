ab575c3d44785c0a57b5b6e573a5768535b6dfe5
abdisag1 <myabdisa@gmail.com>
2022-03-09 21:28:53 +0000

    added amenity.py place.py and user.py

--- models/user.py
+++ models/user.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This is the user class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship("Place", cascade="all", backref="user")
+        reviews = relationship("Review", cascade="all", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
