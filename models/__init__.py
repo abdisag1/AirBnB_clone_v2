5f22e2d5141f8963eb425a63a3de1f78b6b2fd58
MoHack <60172765+Mo258456@users.noreply.github.com>
2022-03-09 22:55:08 +0300

    Few modifications

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Instantiates a storage object.
-->conditional depending of the value of the environment variable
+
+-> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
+   instantiates a database storage engine (DBStorage).
+-> Otherwise, instantiates a file storage engine (FileStorage).
 """
 from os import getenv
 
