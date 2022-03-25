0a9fa2c3813315e453b3548d745159348b4b6af1
abdisag1 <myabdisa@gmail.com>
2022-03-25 09:51:49 +0000

    done task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,9 +3,13 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
+mkdir -p /data
+mkdir /data/web_static
+mkdir /data/web_static/releases
 sudo mkdir /data/web_static/shared/
+mkdir /data/web_static/releases/test
 echo "test my servers" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+rm -f /data/web_static/current && ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hr ubuntu:ubuntu /data/
 redirect="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo sed -i "50s|.*|$redirect|" /etc/nginx/sites-enabled/default
