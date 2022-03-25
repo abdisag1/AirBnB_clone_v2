34e4b1539608f3240edee3bf8f6a2c254a204504
abdisag1 <myabdisa@gmail.com>
2022-03-25 10:15:39 +0000

    done task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,15 +2,15 @@
 #  sets up your web servers for the deployment of web_static
 
 sudo apt-get -y install nginx
-mkdir -p /data
-mkdir /data/web_static
-mkdir /data/web_static/releases
-mkdir /data/web_static/shared
-mkdir /data/web_static/releases/test
-touch /data/web_static/releases/test/index.html
-echo "Deployment Test" > /data/web_static/releases/test/index.html
-rm -f /data/web_static/current && ln -s /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data
+sudo mkdir -p /data
+sudo mkdir /data/web_static
+sudo mkdir /data/web_static/releases
+sudo mkdir /data/web_static/shared
+sudo mkdir /data/web_static/releases/test
+sudo touch /data/web_static/releases/test/index.html
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo rm -f /data/web_static/current && ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data
 redirect="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo sed -i "50s|.*|$redirect|" /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+sudo service nginx start
