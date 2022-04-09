f71ec4c1cf939fe71a323420b6ea54565bb3d5ab
abdisag1 <myabdisa@gmail.com>
2022-04-09 10:51:19 -0700

    fixed 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,19 @@
 script listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
-
 """
+
 from flask import Flask
+
+
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
-def index():
+def hello_hbnb():
     """displays Hellow HBNB!"""
     return "Hello HBNB!"
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
         app.run(host="0.0.0.0")
