3dd2cb9a807cccc2da77cb6ffc54da3fe4f12156
abdisag1 <myabdisa@gmail.com>
2022-04-09 10:45:34 -0700

    init_py must be empty

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,10 @@ Routes:
 
 """
 from flask import Flask
-app = Flsk(__name__)
+app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def index():
     """displays Hellow HBNB!"""
-        return "Hello HBNB!"
+    return "Hello HBNB!"
 if __name__ == '__main__':
         app.run(host="0.0.0.0")
